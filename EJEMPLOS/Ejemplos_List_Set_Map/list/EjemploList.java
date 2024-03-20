b6930a1dc24148972e57db246608bb836abf2f85
ProfeMelola <melola@educa.madrid.org>
2024-03-20 14:11:55 +0100

    actualizado

--- EJEMPLOS/Ejemplos_List_Set_Map/list/EjemploList.java
+++ EJEMPLOS/Ejemplos_List_Set_Map/list/EjemploList.java
@@ -14,123 +14,119 @@ import modelo.Persona;
 
 public class EjemploList {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		//List<Persona> listaPersonas = new ArrayList<>();
-                
-                // LINKEDLIST: Si no tenemos una buena razón para usar linkedlist
-                //En términos de funcionalidad no habría diferencia ninguna
-                /*
-                 La principal diferencia es un tema de implementación ArrayList 
-                implementa la lista con arreglos linkeados, lo que la hace muy 
-                eficiente para recorrer pero ineficiente para insertar o eliminar 
-                elementos que se no encuentren en el final. 
-                En cambio LinkedList es una lista de elementos linkeados 
-                (en realidad es una lista doblemente enlazada) lo que la hace 
-                lenta para recorrer pero sumamente eficiente para insertar 
-                o eliminar elementos.
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-                Por lo tanto ArrayList :
-                - usa internamente un arreglo dinámico para almacenar los elementos.
-                - proporciona una manipulación lenta 
-                - es la mejor opción para almacenar y acceder a datos o elementos consecutivos. 
+        // List<Persona> listaPersonas = new ArrayList<>();
 
-                Por lo tanto LinkedList:
-                - proporciona una manipulación más rápida porque utiliza una lista doblemente enlazada.
-                - se puede utilizar como lista y cola porque implementa interfaz de List, Deque y Queue.
-                - es mejor para manipulación de elementos, es decir, para insertar y eliminar elementos.
-                
-                Por lo tanto si necesitamos agregar elementos consecutivos 
-                y recorrerlos debemos utilizar ArrayList y si en cambio 
-                necesitamos insertar y eliminar valores no consecutivos debemos usar LinkedList.                
-                */
-                
-		List<Persona> listaPersonas = new LinkedList<>(); 
-		
-		listaPersonas.add(new Persona("12345678A", "Pepe", "Perez", LocalDate.of(1990, 1, 2)));
-		listaPersonas.add(new Persona("23456789B", "Juan", "Martínez", LocalDate.of(1991, 2, 3)));
-		listaPersonas.add(new Persona("34567890C", "Ana", "Ramírez", LocalDate.of(1992, 3, 4)));
-		listaPersonas.add(new Persona("45678901D", "María", "López", LocalDate.of(1993, 4, 5)));
-		
-		//Acceso posicional (3ª posición)
-		Persona p = listaPersonas.get(2);
-		System.out.println(p);
-		System.out.println("\n\n");
-		
-		//Recorrer la lista completa
-		for(Persona per : listaPersonas)
-			System.out.println(per);
-		
-		
-		//Añadir un nuevo elemento al final de la lista
-		listaPersonas.add(new Persona("56789012E", "Julio", "Azcárate", LocalDate.of(1994, 5, 6)));
-		
-		//Añadir/modificar un elemento en medio de la lista. Cambio el elemento de la posición 2 (sustituye Ana por Alfonso y Julio está al final)
-		listaPersonas.set(2, new Persona("67890123F", "Alfonso", "García", LocalDate.of(1995, 6, 7)));
-		
-		//Recorrer la lista completa
-		System.out.println("\n\n\n");
-		for(Persona per : listaPersonas)
-			System.out.println(per);
-		
-		//Para ordenar, tenemos que aportar un orden
-		//Será por fecha de nacimiento, a la inversa
-		listaPersonas.sort(new Comparator<Persona>() {
+        // LINKEDLIST: Si no tenemos una buena razón para usar linkedlist
+        // En términos de funcionalidad no habría diferencia ninguna
+        /*
+         * La principal diferencia es un tema de implementación ArrayList
+         * implementa la lista con arreglos linkeados, lo que la hace muy
+         * eficiente para recorrer pero ineficiente para insertar o eliminar
+         * elementos que se no encuentren en el final.
+         * En cambio LinkedList es una lista de elementos linkeados
+         * (en realidad es una lista doblemente enlazada) lo que la hace
+         * lenta para recorrer pero sumamente eficiente para insertar
+         * o eliminar elementos.
+         * 
+         * Por lo tanto ArrayList :
+         * - usa internamente un arreglo dinámico para almacenar los elementos.
+         * - proporciona una manipulación lenta
+         * - es la mejor opción para almacenar y acceder a datos o elementos
+         * consecutivos.
+         * 
+         * Por lo tanto LinkedList:
+         * - proporciona una manipulación más rápida porque utiliza una lista doblemente
+         * enlazada.
+         * - se puede utilizar como lista y cola porque implementa interfaz de List,
+         * Deque y Queue.
+         * - es mejor para manipulación de elementos, es decir, para insertar y eliminar
+         * elementos.
+         * 
+         * Por lo tanto si necesitamos agregar elementos consecutivos
+         * y recorrerlos debemos utilizar ArrayList y si en cambio
+         * necesitamos insertar y eliminar valores no consecutivos debemos usar
+         * LinkedList.
+         */
 
-			@Override
-			public int compare(Persona p1, Persona p2) {
-				return -p1.getFechaNacimiento().compareTo(p2.getFechaNacimiento());
-			}
-			
-		});
-		
-		//Recorremos la lista completa, ya ordenada
-		System.out.println("\n\n\n");
-		for(Persona per : listaPersonas)
-			System.out.println(per);
-		
-		
-                //Buscar
-                Persona aux = new Persona("67890123F", "Alfonso", "García", LocalDate.of(1995, 6, 7));
-                
-                System.out.println("Persona aux:"+aux);
-                Persona alfonso = listaPersonas.get(0);
-                System.out.println("Persona a buscar (Alfonso):"+alfonso);
-                System.out.println("iguales:"+alfonso.equals(aux));
-                
-                // BORRANDO A TRAVÉS DE LA POSICIÓN
-                // Necesito que la clase Persona implemente la interface Comparable o crear un Comparator específico
-                
-                //Collections.sort(listaPersonas,Comparator.naturalOrder());
-                //int pos = Collections.binarySearch(listaPersonas, aux);
-                
-                
-                Comparator<Persona> byDNI = (pa, pb) -> pa.getDni().compareTo(pb.getDni());
-                Collections.sort(listaPersonas,byDNI);
-                //int pos = Collections.binarySearch(listaPersonas, aux,byDNI);
-                int pos = Collections.binarySearch(listaPersonas, aux); //así no se puede salvo si persona implementa Comparable
-                
-                System.out.println("pos:"+pos);
-                
-                if (pos >= 0)
-                    listaPersonas.remove(pos);
-                
-                
-                
-                // BORRANDO OBJETOS (EQUALS)
-                
-                /*if (alfonso.equals(aux))
-                    listaPersonas.remove(aux);*/
-                
-                System.out.println("\n\n\nLista actualizada tras borrar:");
-		for(Persona per : listaPersonas)
-			System.out.println(per);
-		                
-		
-		
-	}
+        List<Persona> listaPersonas = new LinkedList<>();
+
+        listaPersonas.add(new Persona("12345678A", "Pepe", "Perez", LocalDate.of(1990, 1, 2)));
+        listaPersonas.add(new Persona("23456789B", "Juan", "Martínez", LocalDate.of(1991, 2, 3)));
+        listaPersonas.add(new Persona("34567890C", "Ana", "Ramírez", LocalDate.of(1992, 3, 4)));
+        listaPersonas.add(new Persona("45678901D", "María", "López", LocalDate.of(1993, 4, 5)));
+
+        // Recorrer la lista completa
+        System.out.println("Listado completo de personas:");
+        for (Persona per : listaPersonas)
+            System.out.println(per);
+
+        // Acceso posicional (3ª posición)
+        Persona p = listaPersonas.get(2);
+        System.out.println("\nPersona en tercera posición: " + p);
+
+        // Añadir un nuevo elemento al final de la lista
+        listaPersonas.add(new Persona("56789012E", "Julio", "Azcárate", LocalDate.of(1994, 5, 6)));
+
+        // Añadir/modificar un elemento en medio de la lista. Cambio el elemento de la
+        // posición 2 (sustituye Ana por Alfonso y Julio está al final)
+        listaPersonas.set(2, new Persona("67890123F", "Alfonso", "García", LocalDate.of(1995, 6, 7)));
+
+        // Añadir un objeto repetido
+        listaPersonas.add(new Persona("56789012E", "Julio", "Azcárate", LocalDate.of(1994, 5, 6)));
+
+        // Recorrer la lista completa
+        System.out.println("\nNuevo listado de personas:");
+        for (Persona per : listaPersonas)
+            System.out.println(per);
+
+        // Para ordenar, tenemos que aportar un orden
+        // Será por fecha de nacimiento
+        listaPersonas.sort(new Comparator<Persona>() {
+
+            @Override
+            public int compare(Persona p1, Persona p2) {
+                return p1.getFechaNacimiento().compareTo(p2.getFechaNacimiento());
+            }
+
+        });
+
+        // Recorremos la lista completa, ya ordenada
+        System.out.println("\nListado de personas por fecha de nacimiento:");
+        for (Persona per : listaPersonas)
+            System.out.println(per);
+
+        // ------------------------------------------------
+        // BUSCAR
+        // Quiero encontrar a Alfonso....
+        System.out.println("* Buscar a persona:");
+        Persona Alfonso = new Persona("67890123F", "Alfonso", "García", LocalDate.of(1995, 6, 7));
+        System.out.println(Alfonso);
+
+        //int pos = Collections.binarySearch(listaPersonas, Alfonso); // requisito que Persona implements Comparable
+
+        // Comparator<Persona> byNombre = (pa, pb) -> pa.getNombre().compareTo(pb.getNombre());
+        // Collections.sort(listaPersonas, byNombre);
+        // int pos = Collections.binarySearch(listaPersonas, Alfonso, byNombre);
+
+        //System.out.println("posición encontrada:" + pos);
+
+        // BORRANDO OBJETOS (POR POSICIÓN)
+        // if (pos >= 0)
+        //     listaPersonas.remove(pos);
+
+        // BORRANDO OBJETOS (EQUALS)
+        listaPersonas.remove(Alfonso);
+
+
+        System.out.println("\nLista actualizada tras borrar:");
+        for (Persona per : listaPersonas)
+            System.out.println(per);
+
+    }
 
 }
--- EJEMPLOS/Ejemplos_List_Set_Map/set/EjemploSet.java
+++ EJEMPLOS/Ejemplos_List_Set_Map/set/EjemploSet.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -21,43 +22,60 @@ public class EjemploSet {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-                //No se garantiza que se devuelva en el orden de insercción
-		//Set<Persona> juntaDirectiva = new HashSet<>();
-                
-                //Exception in thread "main" java.lang.ClassCastException: class modelo.Persona cannot be cast to class java.lang.Comparable
-                //Para ordenar, Persona debe implementar Comparable
-		Set<Persona> juntaDirectiva = new TreeSet<>(); 
-                
-                //Se devuelve en el orden de insercción
-		//Set<Persona> juntaDirectiva = new LinkedHashSet<>();
-		
-		juntaDirectiva.add(new Persona("23456789B", "Juan", "Martínez", LocalDate.of(1991, 2, 3)));
-		juntaDirectiva.add(new Persona("12345678A", "Pepe", "Perez", LocalDate.of(1990, 1, 2)));
-		juntaDirectiva.add(new Persona("34567890C", "Ana", "Ramírez", LocalDate.of(1992, 3, 4)));
-		juntaDirectiva.add(new Persona("45678901D", "María", "López", LocalDate.of(1993, 4, 5)));
-		
-		//Si tratamos de añadir un elemento repetido...
-                //No da error, simplement no se inserta el nuevo elemento
-		juntaDirectiva.add(new Persona("45678901D", "María", "López", LocalDate.of(1993, 4, 5)));
 
-                
-                System.out.println("* Personas en orden ascendente:");
-                //Comprobamos que al listarlos todos, no aparece duplicado
-		for(Persona p : juntaDirectiva)
+		// --------------------------------------------------------------------------------
+		// -------------------- DECLARACIÓN DE DIFERENTES SET -----------------------------
+		// --------------------------------------------------------------------------------
+		//1. HashSet
+		// No se garantiza que se devuelva en el orden de inserción
+		//Set<Persona> juntaDirectiva = new HashSet<>();
+
+		// 2. TreeSet
+		// Exception in thread "main" java.lang.ClassCastException: class modelo.Persona
+		// cannot be cast to class java.lang.Comparable
+		// Para ordenar, Persona debe implementar Comparable
+		Set<Persona> juntaDirectiva = new TreeSet<>();
+
+		//3. LinkedHashSet
+		// Se devuelve en el orden de inserción
+		// Set<Persona> juntaDirectiva = new LinkedHashSet<>();
+		// --------------------------------------------------------------------------------
+
+		// --------------------------------------------------------------------------------
+		// ----------------------- INICIALIZAMOS EL SET CON OBJETOS -----------------------
+		// --------------------------------------------------------------------------------
+
+		juntaDirectiva.add(new Persona("11111111B", "Juan", "Martínez", LocalDate.of(1991, 2, 3)));
+		juntaDirectiva.add(new Persona("11111111A", "Pepe", "Perez", LocalDate.of(1990, 1, 2)));
+		juntaDirectiva.add(new Persona("11111111C", "Ana", "Ramírez", LocalDate.of(1992, 3, 4)));
+		juntaDirectiva.add(new Persona("11111111D", "María", "López", LocalDate.of(1993, 4, 5)));
+
+
+		// AÑADIR OBJETO REPETIDO
+		// Si tratamos de añadir un elemento repetido...
+		// No da error, simplement no se inserta el nuevo elemento
+		juntaDirectiva.add(new Persona("11111111D", "María", "López", LocalDate.of(1993, 4, 5)));
+
+		System.out.println("* Listado de personas:");
+		// Comprobamos que al listarlos todos, no aparece duplicado
+		// Observa que dependiendo del tipo de Set que utilices, el listado es diferente...
+		for (Persona p : juntaDirectiva)
 			System.out.println(p);
+
+			System.out.println("* Listado de personas en orden descendente:");
+		System.out.println("\t* Con descendingIterator:");
+		Iterator<Persona> it = ((TreeSet<Persona>) juntaDirectiva).descendingIterator();
+		while(it.hasNext())
+			System.out.println(it.next());
+
+		System.out.println("\t* Como un ArrayList:");
+		List<Persona> lista = new ArrayList<>(juntaDirectiva); // convierto un Set a un List
+		lista.sort(Comparator.reverseOrder());
+		//lista.sort(Collections.reverseOrder()); // otra forma
+		// lista.sort( (pa,pb) -> pa.getNombre().compareTo(pb.getNombre())); // un comparator solo por nombre...
+		lista.forEach(System.out::println);
 		
-                //Iterator it = juntaDirectiva.descendingIterator(); //está en java doc
-                ArrayList<Persona> lista = new ArrayList(juntaDirectiva);
-                //lista.sort( (pa,pb) -> pa.getNombre().compareTo(pb.getNombre()));
-                lista.sort(Comparator.reverseOrder());
-                
-                System.out.println("* Personas en orden descendente:");
-                lista.forEach(System.out::println);
-                //lista.sort(Collections.reverseOrder());
-                
-		
-		
+
 	}
 
 }
