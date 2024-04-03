85a5dd99a919b0d6f449c60cb28ae72e23e12c9f
ProfeMelola <melola@educa.madrid.org>
2024-04-03 16:20:24 +0200

    organizado

--- EJEMPLOS/Ejemplos_List_Set_Map/list/EjemploListEnteros.java
+++ EJEMPLOS/Ejemplos_List_Set_Map/list/EjemploListEnteros.java
@@ -6,6 +6,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/*
+ * CONCEPTOS:
+ * Repaso de ArrayList
+ * Interface List
+ * Nuevos métodos
+ * Factory methods
+ * Concepto de inmutabilidad
+ * Ordenaciones
+ */
 public class EjemploListEnteros {
     public static void main(String[] args) {
 
@@ -13,9 +22,9 @@ public class EjemploListEnteros {
         System.out.println("**EJEMPLOS DE ORDENACIÓN DE ARRAYLIST DE ENTEROS *");
         System.out.println("**************************************************");
 
-        System.out.println("**************************************************");
-        System.out.println("** ARRAYLIST ENTEROS 1 *");
-        System.out.println("**************************************************");
+        System.out.println("*****************************************************");
+        System.out.println("** ARRAYLIST ENTEROS 1: creado de forma tradicional *");
+        System.out.println("*****************************************************");
 
         // Forma tradicional
         ArrayList<Integer> listInt = new ArrayList<>();
@@ -27,53 +36,80 @@ public class EjemploListEnteros {
         listInt.add(1);
         listInt.add(7);
 
-        // Factory Methods para poder crear colecciones
-        List<Integer> listInt2 = List.of(1,2,5,7,9,19);
-        // Tengo que hacer casting si quiero un ArrayList en vez de un List
-        ArrayList<Integer> listInt3 = (ArrayList<Integer>) List.of(1,2,5,7,9,19);
+        System.out.println("* Lista de enteros tal cual:");
+        listInt.forEach(System.out::println);
 
+        System.out.println("*****************************************************");
+        System.out.println("** ARRAYLIST ENTEROS 2: Factory method List.of      *");
+        System.out.println("*****************************************************");
+
+        // Factory Methods para poder crear colecciones
+        // List.of devuelve una lista inmutable, no una instancia de ArrayList. 
+        // La inmutabilidad significa que no puedes modificar la lista después de crearla
+        // NO SE PUEDE ORDENAR NI CREAR NI BORRAR ELEMENTOS!!!!!!!!!!
+        List<Integer> listInt2 = List.of(3,4,2,6,5,1, 7);
+
+        //listInt2.add(666); //Da error
+        //Collections.sort(listInt2); //Da error
+
+        // Si quieres tener una lista mutable
+        ArrayList<Integer> listInt3 = new ArrayList<>(List.of(3,4,2,6,5,1, 7));
+
+        // Para los ejemplos vamos a usar listInt3 (serviría también con listInt)
         System.out.println("* Lista de enteros no ordenada (for..)");
-        for (Integer i : listInt2) {
+        for (Integer i : listInt3) {
             System.out.println(i);
         }
 
         System.out.println("* Lista de enteros no ordenada (forEach)");
-        listInt2.forEach(System.out::println);
+        listInt3.forEach(System.out::println);
 
         System.out.println("* Lista de enteros no ordenada (->)");
-        listInt2.forEach( i -> System.out.println(i));
-        listInt2.forEach( (i) -> {
-            System.out.println(i);
-        });
-
-        System.out.println("* Lista de enteros inversa:");
-        Collections.reverse(listInt);
-        listInt2.forEach(System.out::println);
+        listInt3.forEach( i -> System.out.println(i));
+        // listInt2.forEach( (i) -> {
+        //     System.out.println(i);
+        // });
 
         System.out.println("* Lista de enteros ordenada por defecto (ascendente):");
 
         System.out.println("\tForma 1: con Collections.sort");
-        Collections.sort(listInt);
-        listInt2.forEach(System.out::println);
+        Collections.sort(listInt3); // no da error porque Collection sort devuelve una copia mutable
+        listInt3.forEach(System.out::println);
+
+        System.out.println("\tForma 2: con sort del ArrayList");
+        listInt3.sort(Comparator.naturalOrder()); // da error porque es inmutable
+        listInt3.forEach(System.out::println);        
 
         System.out.println("* Lista de enteros ordenada descendente:");
-        // Forma 1
+
         System.out.println("\tForma 1: con Collections.sort");
-        Collections.sort(listInt2, Comparator.reverseOrder());
-        listInt2.forEach(System.out::println);
+        Collections.sort(listInt3, Comparator.reverseOrder());
+        listInt3.forEach(System.out::println);
 
-        // Forma 2
         System.out.println("\tForma 2 con sort del ArrayList");
-        listInt2.sort(Comparator.reverseOrder());
-        listInt2.forEach(System.out::println);
+        listInt3.sort(Comparator.reverseOrder());
+        listInt3.forEach(System.out::println);
 
-        System.out.println("**************************************************");
-        System.out.println("** ARRAYLIST ENTEROS 2 *");
-        System.out.println("**************************************************");
+        System.out.println("\tForma 3 con Collections.reverse");
+        Collections.reverse(listInt3);
+        listInt3.forEach(System.out::println);
+
+
+        System.out.println("*****************************************************");
+        System.out.println("** ARRAYLIST ENTEROS 4: Factory method Arrays.asList*");
+        System.out.println("*****************************************************");
+        /*
+         * La lista devuelta por Arrays.asList() no es completamente mutable 
+         * en el sentido de que no puedes agregar ni eliminar elementos de la lista, 
+         * pero sí puedes modificar los elementos existentes en ella.
+         */
 
         List<Integer> listInt4 = Arrays.asList(4, 5, 1, 2, 8, 9, 6);
 
-        // El resto igual .....
+        listInt4.sort(Comparator.naturalOrder());
+        listInt4.forEach(System.out::println);
+
+        //listInt4.add(666); //Da error
 
     }
 
